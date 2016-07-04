f1193adc1e492685ab7a6f1250660516cabd76d0
phquhai <phquhai@yahoo.com>
2016-07-04 22:09:38 +0700

    Create pushcrew.js

---
+++ pushcrew.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+(function(p,u,s,h){
+    p._pcq=p._pcq||[];
+    p._pcq.push(['_currentTime',Date.now()]);
+    s=u.createElement('script');
+    s.type='text/javascript';
+    s.async=true;
+    s.src='https://cdn.pushcrew.com/js/a421a23639630c94a21176948eebe796.js';
+    h=u.getElementsByTagName('script')[0];
+    h.parentNode.insertBefore(s,h);
+})(window,document);
+</script>
