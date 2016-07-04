2d38386b32df138cc1168573fa5fe2417151e6a1
phquhai <phquhai@yahoo.com>
2016-07-04 22:11:25 +0700

    Create statcounter.js

---
+++ statcounter.js
@@ -0,0 +1,14 @@
+var sc_project=10764963; 
+var sc_invisible=1; 
+var sc_security="ed14ed9a"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="click tracking"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="http://c.statcounter.com/10764963/0/ed14ed9a/1/"
+alt="click tracking"></a></div>
